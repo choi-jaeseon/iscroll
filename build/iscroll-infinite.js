43741cadc0494401c9e471bf0d79b2f30c3acdca
YongWoo Jeon <yongwoo.jeon@navercorp.com>
2016-12-19 15:57:56 +0900

    bugfix for chrome55

--- build/iscroll-infinite.js
+++ build/iscroll-infinite.js
@@ -86,7 +86,8 @@ var utils = (function () {
 		hasTransform: _transform !== false,
 		hasPerspective: _prefixStyle('perspective') in _elementStyle,
 		hasTouch: 'ontouchstart' in window,
-		hasPointer: !!(window.PointerEvent || window.MSPointerEvent), // IE10 is prefixed
+		// hasPointer: !!(window.PointerEvent || window.MSPointerEvent), // IE10 is prefixed
+		hasPointer: false, // bugfix
 		hasTransition: _prefixStyle('transition') in _elementStyle
 	});
 
